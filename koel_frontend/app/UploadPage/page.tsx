a9083541ebc2681cfc94a3b19bf01259df9e7a05
TheeObserverr <162690828+TheeObserverr@users.noreply.github.com>
2024-06-06 22:44:53 +0800

    Front End for Koel - Upload and play audio

---
+++ koel_frontend/app/UploadPage/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import { fetchAnimals, fetchUsers } from '@/app/lib/dataFetching';
+import Map from "@/app/Components/map/map";
+import Filters from "@/app/Components/Filter/map_filter"
+import BaseLayout from '@/app/Components/BaseLayout/BaseLayout';
+import "./Upload.css"
+import AudioPlayer from '@/app/Components/AudioPlayer/AudioPlayer';
+import SearchIcon from "@/app/Icons/search-icon.svg"
+import Image from 'next/image';
+
+
+const UploadPage: React.FC = () => {
+  // const audioUrl = '/STRAW-HEADED BULBUL.mp3'; 
+  const [fileName, setFileName] = useState("");
+  const [loading, setLoading] = useState(false);
+  
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
+  const [fetchedImg, setFetchedImg] = useState(null);
+
+  const handleFileChange = (event:any) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    console.log(file);
+    setFileName(file.name);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('audio', selectedFile);
+
+    try {
+      setUploading(true);
+      const response = await fetch('http://127.0.0.1:8000/api/predict_audio', {
+            method: 'POST',
+            body: formData,
+      });
+  
+      const data = await response.json();
+      console.log('response data:', data);
+      
+      return data;
+
+    } catch (error) {
+      console.error('Error detecting:', error);
+      return []
+
+    } finally {
+      setUploading(false);
+    }
+  };  
+
+
+
+  const handleSearch = () => {
+    // Implement upload functionality here
+    console.log('Search button clicked');
+  };
+
+  const handleDownload = () => {
+    // Implement download functionality here
+    console.log('Download button clicked');
+  };
+
+
+
+  return (
+    <div>
+      <BaseLayout>
+      <div>
+        <div className="text-5xl font-bold mb-10">Recording Page</div>
+        <div className="">
+          <div className="FileExplorer flex items-center">
+          <div className="SearchForm">
+            <div className="SearchBar">
+              <p className="text-lg">{fileName}</p>
+              
+
+            </div>
+
+            </div>
+            <div className="ml-4 flex justify-center items-center">
+            <label className="button_search">
+                <input type="file" hidden accept="audio/*" onChange={handleFileChange}/> 
+                <Image src={SearchIcon} className="search_logo" alt="Hamburger Icon"/>
+            </label>
+
+              <button className="button" onClick={handleUpload} disabled={uploading}>
+                {uploading ? 'Detecting...' : 'Detect'}
+              </button>
+              
+              <button 
+                className="button"
+                onClick={handleDownload}
+              >
+                Download CSV
+              </button>
+          </div>
+
+          </div>
+
+        </div>
+
+        <div className='AudioPlayer'>
+          Sound Stuff
+
+          {selectedFile && <AudioPlayer audioUrl={URL.createObjectURL(selectedFile)} />}
+        </div>
+        </div>
+      </BaseLayout>
+    </div>
+  );
+};
+
+  
+  export default UploadPage;
---
+++ koel_frontend/src/app/Components/AniSpecies/AniSpecies.tsx
@@ -0,0 +1,11 @@
+import { useState, useRef, useEffect } from 'react';
+
+
+export default function AniSpecies() {
+  return (
+
+    <div>
+      hellos
+    </div>
+  );
+}
